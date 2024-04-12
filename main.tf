07aa4b4070aa9ba02ef5768bc1323503fd7cafcf
Jumpei Usui <jumpeisoccerguitar@gmail.com>
2024-04-12 06:56:14 +0000

    feat: terraform initを実行

---
+++ main.tf
@@ -0,0 +1,15 @@
+terraform {
+  backend "s3" {
+    bucket = "jumpei-usui-terraform-state"
+    key    = "terraform.tfstate"
+    region = "ap-northeast-1"
+
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+  required_version = ">=1.2.0"
+}
