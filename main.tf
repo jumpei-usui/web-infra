a246b8fdac5ce28cf0128011ef6e57cafce1c529
Jumpei Usui <jumpeisoccerguitar@gmail.com>
2024-04-18 00:47:10 +0000

    feat: データベースとAPIのモジュールをメインに追加

--- main.tf
+++ main.tf
@@ -21,3 +21,19 @@ provider "aws" {
 module "network" {
   source = "./modules/network"
 }
+
+module "database" {
+  source             = "./modules/database"
+  engine_version     = "8.0.mysql_aurora.3.04.2"
+  cluster_identifier = "${var.product_name}-database"
+  vpc_id             = module.network.vpc_id
+  subnet_ids         = module.network.private_subnet_ids
+  subnet_id          = module.network.public_subnet_id
+  key_name           = "web-infra"
+  cidr_blocks        = var.cidr_blocks
+}
+
+module "api" {
+  source       = "./modules/api"
+  product_name = var.product_name
+}
