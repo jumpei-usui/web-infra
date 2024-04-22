30ae000d9ae4720fc8c3baad880b357c110cb12f
Jumpei Usui <jumpeisoccerguitar@gmail.com>
2024-04-22 17:31:20 +0000

    feat: オートスケーリングの設定を追加

--- modules/api/main.tf
+++ modules/api/main.tf
@@ -316,3 +316,30 @@ resource "aws_route53_record" "cloudfront_ipv6" {
     evaluate_target_health = false
   }
 }
+
+resource "aws_appautoscaling_target" "ecs_target" {
+  max_capacity       = 4
+  min_capacity       = 2
+  resource_id        = "service/${aws_ecs_cluster.this.name}/${aws_ecs_service.this.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+}
+
+resource "aws_appautoscaling_policy" "ecs_policy" {
+  name               = "ECSServiceAverageCPUUtilization"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.ecs_target.resource_id
+  scalable_dimension = aws_appautoscaling_target.ecs_target.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ecs_target.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
+    }
+
+    target_value       = 70
+    scale_out_cooldown = 300
+    scale_in_cooldown  = 300
+    disable_scale_in   = false
+  }
+}
