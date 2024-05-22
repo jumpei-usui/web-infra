7734041c3cde032dc1277b1acf493160a372674b
Jumpei Usui <jumpeisoccerguitar@gmail.com>
2024-05-22 14:59:12 +0000

    feat: メインモジュールで指定する変数の修正

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "usui-terraform-state"
+    bucket = "usuijuice-terraform-state"
     key    = "terraform.tfstate"
     region = "us-east-1"
 
@@ -11,6 +11,7 @@ terraform {
       version = "~> 5.45"
     }
   }
+
   required_version = ">=1.8.0"
 }
 
@@ -38,41 +39,42 @@ module "frontend" {
   source              = "./modules/frontend"
   product_name        = var.product_name
   domain_name         = var.domain_name
-  acm_certificate_arn = module.certificate.frontend_acm_certificate_arn
   zone_id             = module.certificate.zone_id
+  acm_certificate_arn = module.certificate.frontend_acm_certificate_arn
   web_acl_arn         = module.firewall.web_acl_arn
 }
 
 module "database" {
-  source             = "./modules/database"
-  engine_version     = "8.0.mysql_aurora.3.04.2"
-  cluster_identifier = "${var.product_name}-database"
-  vpc_id             = module.network.vpc_id
-  subnet_ids         = module.network.private_subnet_ids
-  subnet_id          = module.network.public_subnet_id
-  key_name           = "web-infra"
-  cidr_blocks        = var.cidr_blocks
+  source                     = "./modules/database"
+  engine_version             = "8.0.mysql_aurora.3.04.2"
+  cluster_identifier         = "${var.product_name}-database"
+  vpc_id                     = module.network.vpc_id
+  subnet_ids                 = module.network.private_subnet_ids
+  subnet_id                  = module.network.public_subnet_id
+  private_subnet_cidr_blocks = module.network.private_subnet_cidr_blocks
 }
 
 module "api" {
   source                        = "./modules/api"
   product_name                  = var.product_name
-  vpc_id                        = module.network.vpc_id
-  vpc_default_security_group_id = module.network.vpc_default_security_group_id
-  public_subnet_ids             = module.network.public_subnet_ids
-  private_subnet_ids            = module.network.private_subnet_ids
+  domain_name                   = var.domain_name
+  zone_id                       = module.certificate.zone_id
   alb_acm_certificate_arn       = module.certificate.alb_acm_certificate_arn
   api_acm_certificate_arn       = module.certificate.api_acm_certificate_arn
-  zone_id                       = module.certificate.zone_id
-  domain_name                   = var.domain_name
-  rds_cluster_endpoint          = module.database.rds_cluster_endpoint
+  web_acl_arn                   = module.firewall.web_acl_arn
+  public_subnet_ids             = module.network.public_subnet_ids
+  private_subnet_ids            = module.network.private_subnet_ids
+  vpc_id                        = module.network.vpc_id
   region                        = var.region
   account_id                    = data.aws_caller_identity.this.account_id
-  web_acl_arn                   = module.firewall.web_acl_arn
+  rds_cluster_endpoint          = module.database.rds_cluster_endpoint
+  vpc_default_security_group_id = module.network.vpc_default_security_group_id
 }
 
 module "auth" {
-  source       = "./modules/auth"
-  product_name = var.product_name
-  metadata_url = var.metadata_url
+  source        = "./modules/auth"
+  product_name  = var.product_name
+  metadata_url  = var.metadata_url
+  callback_urls = ["http://localhost:3000", "https://${var.domain_name}"]
+  logout_urls   = ["http://localhost:3000", "https://${var.domain_name}"]
 }
